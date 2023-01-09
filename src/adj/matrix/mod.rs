aa4d14dff3f49d9bf26cace4704ec1caaf8b5a85
FIA <ayrat.galiullin@icloud.com>
2023-01-09 21:03:36 +0300

    the Dijkstra algo

--- src/adj/matrix/mod.rs
+++ src/adj/matrix/mod.rs
@@ -2,6 +2,7 @@ pub mod row;
 
 use row::Row;
 use std::fmt::{Display, Formatter, Result};
+use std::i32::MAX;
 use std::ops::{Index, IndexMut};
 use std::vec;
 
@@ -44,13 +45,11 @@ impl Matrix {
     /// Adds a directed edge without weight
     pub fn add_directed_edge(&mut self, n: usize, m: usize) {
         self.add_weighted_edge(n, m, 1);
-        self.add_weighted_edge(m, n, -1);
     }
 
     /// Adds a directed edge with weight
     pub fn add_w_directed_edge(&mut self, n: usize, m: usize, w: i32) {
         self.add_weighted_edge(n, m, w);
-        self.add_weighted_edge(m, n, -w);
     }
 
     /// Adds an undirected edge
@@ -83,6 +82,21 @@ impl Matrix {
     }
 }
 
+impl Matrix {
+    // Calculates a node with the smallest distance for the Dijkstra algorithm
+    pub fn min_distance(&self, dists: &mut Vec<i32>, done: &mut Vec<bool>) -> usize {
+        let mut min = MAX;
+        let mut min_node = 0;
+        for v in 0..self.n {
+            if !done[v] && dists[v] <= min {
+                min = dists[v];
+                min_node = v;
+            }
+        }
+        return min_node;
+    }
+}
+
 impl Display for Matrix {
     fn fmt(&self, f: &mut Formatter) -> Result {
         let mut matrix = String::new();
@@ -99,9 +113,9 @@ impl Display for Matrix {
 }
 
 impl Matrix {
-    pub fn bfs(&self, starter_node: usize, target_node: usize) -> bool {
+    pub fn bfs(&self, start_node: usize, target_node: usize) -> bool {
         let mut queue = vec![];
-        queue.push(starter_node);
+        queue.push(start_node);
         let mut searched = vec![];
         while !queue.is_empty() {
             let node = queue.remove(0);
@@ -119,5 +133,39 @@ impl Matrix {
             }
         }
         return false;
+    }
+
+    pub fn dijsktra(&self, start_node: usize, target_node: usize) -> Option<(i32, Vec<usize>)> {
+        let mut dists: Vec<i32> = vec![MAX; self.n];
+        let mut done: Vec<bool> = vec![false; self.n];
+        dists[start_node] = 0;
+        let mut path: Vec<usize> = vec![0; self.n];
+        for _ in 0..(self.n - 1) {
+            let min_node = self.min_distance(&mut dists, &mut done);
+            done[min_node] = true;
+            for adj_node in 0..self.n {
+                let not_zero = self[min_node][adj_node] != 0;
+                let new_dist = dists[min_node] + self[min_node][adj_node];
+                if !done[adj_node]
+                    && not_zero
+                    && dists[min_node] != MAX
+                    && new_dist < dists[adj_node]
+                {
+                    path[adj_node] = min_node;
+                    dists[adj_node] = new_dist;
+                }
+            }
+        }
+        if dists[target_node] == MAX {
+            return None;
+        }
+        let mut target_path = path[target_node];
+        let mut final_path = vec![target_node];
+        while !final_path.contains(&0) {
+            final_path.push(target_path);
+            target_path = path[target_path];
+        }
+        final_path.reverse();
+        return Some((dists[target_node], final_path));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 use graphula::adj::matrix::Matrix;
 
 fn main() {
-    // Node 0 - Me
-    // Node 1 - Ruslan
-    // Node 2 - Maksim
-    // Node 4 - Marat
-    // Node 3 - Peter
-    // Node 5 - Sveta
+    // Node 0 - Home
+    // Node 1 - A
+    // Node 2 - B
+    // Node 3 - C
+    // Node 4 - D
+    // Node 5 - Work
     let mut graph: Matrix = Matrix::new(6, 0);
-    graph.add_directed_edge(0, 1);
-    graph.add_directed_edge(0, 2);
-    graph.add_directed_edge(1, 4);
-    graph.add_directed_edge(2, 3);
-    graph.add_directed_edge(3, 5);
-    graph.bfs(0, 4);
+    graph.add_w_directed_edge(0, 1, 2);
+    graph.add_w_directed_edge(0, 2, 1);
+    graph.add_w_directed_edge(1, 3, 6);
+    graph.add_w_directed_edge(1, 4, 2);
+    graph.add_w_directed_edge(2, 3, 2);
+    graph.add_w_directed_edge(2, 4, 7);
+    graph.add_w_directed_edge(3, 5, 10);
+    graph.add_w_directed_edge(4, 5, 2);
+    let res = graph.dijsktra(0, 5);
+    if res.is_some() {
+        let res = res.unwrap();
+        println!("Weights sum: {:?}", res.0);
+        println!("Path: {:?}", res.1);
+    }
 }
